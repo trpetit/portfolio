c3c4af17ca2d05922645c4424d2df8090f8006b9
Tristan Petit <ton.email@example.com>
2025-03-02 18:05:20 +0100

    refacto + new design

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Effet smooth scroll pour les liens du menu
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 60,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
